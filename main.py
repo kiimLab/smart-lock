0875d773db1f5c82ce139432e19655f7e8f7bf7b
syu-kwsk <wataru1165kwsk@gmail.com>
2021-06-18 19:14:45 +0900

    Move read csv out of loop

--- main.py
+++ main.py
@@ -9,16 +9,17 @@ from servo import Servo
 def read_card():
     clf = nfc.ContactlessFrontend('usb') # デバイスの中から１番最初に見つかったものに接続
     print(clf) #デバイスを表示
-    servo = Servo()
+
+    df = pd.read_csv("~/Desktop/name.csv") # csvの読み込み
+    df_id = list(df["ID"])
+
+    servo = Servo() # サーボモータのセットアップ
 
     while True:
         tag = clf.connect(rdwr={'on-connect': lambda tag: False})
         tag_print = str(tag).split()
 
         print(tag_print)
-
-        df = pd.read_csv("~/Desktop/name.csv")
-        df_id = list(df["ID"])
 
         user_id = tag_print[4].replace("ID=","")
         print(user_id)
